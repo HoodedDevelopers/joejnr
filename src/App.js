431e3cd65f8b8fc7cfeb5915f7eaf5fd76f61985
Liam Storkey <liam.storkey@bulletproof.net>
2018-10-15 07:55:44 +1100

    FEAT: basic structure worked out

--- src/App.js
+++ src/App.js
@@ -5,7 +5,43 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        App
+        <div className="page-grid">
+          <div className="main">
+            <div className="content">
+              <div className="content-bold">
+                Three Generations of chicken lovers means we're pretty good at what we do but we'll let our food do the talking (and the walking).
+              </div>
+              <div className="content-light">
+                Burgers, Southern Style Fried Chicken, Loaded Fries, Milkshakes, Salads...
+              </div>
+            </div>
+            <div className="footer">
+              <div className="logo">LOGO</div>
+              <div className="footer-text">
+                <div className="fc-one">
+                  <div className="adress-one">22 Hope Street</div>
+                  <div className="address-two">Happytown 2222</div>
+                </div>
+                <div className="fc-two">
+                  <div className="email">email</div>
+                  <div className="phone">phone</div>
+                </div>
+                <div className="fc-two">facebook instagram</div>
+              </div>
+              <div className="menu-btn"></div>
+            </div>
+          </div>
+          <div className="aside">
+            <div className="img-con ic-one">
+              <div className="img"></div>
+              img one
+            </div>
+            <div className="img-con ic-two">
+              <div className="img"></div>
+              img two
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
