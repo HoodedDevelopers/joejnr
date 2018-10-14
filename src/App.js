a4c43b6f069129a9aa9eb6a75624b8cbbf0cc063
Liam Storkey <liam.storkey@bulletproof.net>
2018-10-15 07:06:28 +1100

    FEAT: reset to blank for New Design

--- src/App.js
+++ src/App.js
@@ -1,42 +1,11 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="griditem item-1 content">
-          <div className="content-chicken">chicken</div>
-          <div className="content-text">
-            Have a speil here about the store and waht it stands for and what it does in an emotional and engaging manner
-          </div>
-        </div>
-        <div className="griditem item-2 logo">LOGO</div>
-        <div className="griditem item-3 menu">
-          <div className="menu-items">
-            <div className="menu-item">BURGERS,</div>
-            <div className="menu-item">FRIED CHICKEN,</div>
-            <div className="menu-item">LOADED FRIES,</div>
-            <div className="menu-item">MILKSHAKES,</div>
-            <div className="menu-item ml">SALADS</div>
-          </div>
-          <div className="footer">
-            <div className="address">
-              <div className="address-line">Address 22 Hope street</div>
-              <div className="address-line">Happytown 2222</div>
-              <div className="address-line">Tel: 02 9875 5432</div>
-              <div className="address-line">Email: hello@joejnr.com.au</div>
-            </div>
-            <div className="social">
-              <div className="facebook"><i class="fab fa-facebook-square"></i></div>
-              <div className="ig"><i class="fab fa-instagram"></i></div>
-            </div>
-          </div>
-          <div className="button">
-            <img src="" alt=""/>
-          </div>
-        </div>
+        App
       </div>
     );
   }
