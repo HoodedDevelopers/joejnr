6969aa40ddfc6222622f6b92cc9b5d697fe4307d
Liam Storkey <storkey.liam@gmail.com>
2018-10-22 08:43:10 +1100

    FEAT: Final fix ups from Justin

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import Logo from './assets/logo.svg';
 import Menu from './assets/menu.png';
+import MenuPDF from './assets/menu.pdf';
 import Meal from './assets/box.jpeg';
 import Burger from './assets/burger.jpeg';
 
@@ -16,7 +17,7 @@ class App extends Component {
                 Three Generations of chicken lovers means we're pretty good at what we do but we'll let our food do the talking (and the walking).
               </div>
               <div className="content-light">
-                Burgers, Southern Style Fried Chicken, Loaded Fries, Milkshakes, Salads...
+                Burgers, Southern Style Fried Chicken, Salads...
               </div>
             </div>
             <div className="footer">
@@ -25,11 +26,11 @@ class App extends Component {
               </div>
               <div className="footer-text">
                 <div className="fc-one">
-                  <div className="adress-one">22 Hope Street</div>
-                  <div className="address-two">Happytown 2222</div>
+                  <div className="adress-one">Shop GO1/580 580 Princess Highway</div>
+                  <div className="address-two">Kirawee 2232</div>
                 </div>
                 <div className="fc-two">
-                  <div className="phone">Tel: 02 9876 5432</div>
+                  <div className="phone">Tel: 0403 241 000</div>
                   <div className="email">Email: hello@joejnr.com.au</div>
                 </div>
                 <div className="fc-three">
@@ -38,7 +39,9 @@ class App extends Component {
                 </div>
               </div>
               <div className="menu-btn">
-                <img src={Menu} alt=""/>
+                <a href={MenuPDF}>
+                  <img src={Menu} alt=""/>
+                </a>
               </div>
             </div>
           </div>
